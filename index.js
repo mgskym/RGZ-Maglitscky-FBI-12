c33751c6265c33f57411255e45af51e81e87e8d9
mgskym <mihaillel@yandex.ru>
2023-05-06 15:51:25 +0700

    Логотип, стили

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-let test = 'Привет!';
-console.log(test);
+// Расчёт варианта:
+
+console.log('Вариант:' + 0x355918deca1ade10f34bb2c9d515aef5 % 155);
+
+// Расчёт меню:
+
+console.log('Меню:' + 0x355918deca1ade10f34bb2c9d515aef5 % 8);
+
